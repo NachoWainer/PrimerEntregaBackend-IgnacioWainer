ebd7386376d43949772d1227d269f721ab4550c5
Ignacio Wainer <nachowainer@gmail.com>
2023-06-15 19:26:32 -0300

    fix bugs + status message

--- src/cartManager.js
+++ src/cartManager.js
@@ -28,56 +28,60 @@ export class CartManager{
             else cart.id =  content.length + 1;
             content.push(cart)
             await fs.promises.writeFile(this.path,JSON.stringify(content,null,'\t'))
+            return {status:202,message:"Carrito agregado",value:[]}
         }catch(error){
             const arr = [] 
             arr.push(cart)
             await fs.promises.writeFile(this.path, JSON.stringify(arr, null, '\t'));
+            return {status:202,message:"Carrito agregado",value:[]}
+
         }
       
     }
 
     getCartById = async (cartId) =>{
+
         try {
             const data = await fs.promises.readFile(this.path,'utf-8')
             const carts = JSON.parse(data)
-            const cart =carts.find(e => e.id === cartId)
-            if(cart) return cart
+            const value = carts.find(e => e.id === cartId)
+            if(value) {
+                return {status:202,message:"Producto encontrado",data:value}}
             else {
-                console.log("No hay carrito con ese ID 404")
-                return 
+                return {status:404,message:"No hay productos con ese ID",data:[]}
         }
         } catch (error) {
-            console.error("No existen Carritos 204")
-            return  
-            
-        }
-    }
+            return{status:404,message:"No existen productos",data:[]}}
+    
+}
 
     addProductToCartIdById = async(CartId,ProductId) =>{
         try{
             const data = await fs.promises.readFile(this.path, 'utf-8')
             const carts = JSON.parse(data)
-            console.log(carts)
             if (!carts.find(cart=>cart.id === CartId)){ 
-                console.log("No existe carrito con ese Id 404")
-                return
+                return {status:404,message:"No hay carrito con ese ID",value:[]}
             }
             else {
                 const cart = carts[carts.findIndex(e=>e.id === CartId)]
-                console.log(cart)
+                
                 const product = cart.products.find(e => e.id === ProductId)
-                console.log(product)
+                let status, message
+                
                 if (product !== undefined){ 
                 product.quantity += 1
-                console.log("cantidad actualizada 202")
+                status = 202
+                message = "cantidad actualizada"
                 }
                 else {
                     cart.products.push({id:`${ProductId}`, quantity:1})
-                    console.log("Producto agregado al carrito 202" + CartId)
+                    status = 202
+                    message = "Producto agregado al carrito con Id"+" "+`${CartId}`
                 }
                 await fs.promises.writeFile(this.path, JSON.stringify(carts,null,"\t"))
+                return{status:status,message:message,value:[]}
                 }
-                return
+                
             } 
         catch(error){
             console.log(error)
--- src/productManager.js
+++ src/productManager.js
@@ -21,8 +21,7 @@ export class ProductManager{
         )=>{
             if (title === undefined || description === undefined || status === undefined || category === undefined || price === undefined ||
                 code === undefined || stock === undefined) {
-              console.error("Los productos a agregar deben incluir 7 campos: title, description, price, code, stock. El campo thumbnail es opcional");
-              return;
+                return {status:412,message:"Los productos a agregar deben incluir 7 campos: title, description, price, code, stock. El campo thumbnail es opcional",value:[]};
             }
             const product= new Product(
                 1,
@@ -40,18 +39,19 @@ export class ProductManager{
             const content = JSON.parse(data);
         
             if (content.find(element => element.code === code)) {
-                console.error("el codigo ya esta en uso 403")
-                return
+                return{stats:403,message:"el codigo ya esta en uso",value:[]}
             }
             if (content.length === 0)  product.id = 1 
             else product.id =  content.length + 1;
             content.push(product)
- 
             await fs.promises.writeFile(this.path,JSON.stringify(content,null,'\t'))
+            return{status:202,message:"Producto agregado",value:[]}
         }catch(error){
             const arr = [] 
             arr.push(product)
             await fs.promises.writeFile(this.path, JSON.stringify(arr, null, '\t'));
+            return{stats:202,message:"Producto agregado",value:[]}
+
         }
       
     }
@@ -60,27 +60,27 @@ export class ProductManager{
         try{
             const data = await fs.promises.readFile(this.path,'utf-8')  
             const product = JSON.parse(data)
-            return product
+            return {status:202,message:"Solicitud aceptada",value:product}
             }
         catch(error){
-            console.error("No existen productos 404")
-            return 
+            return {status:404,message:"No existen productos",value:[]}
             }
         }
 
     
-    getProductById = async (productId) =>{//LISTO
+    getProductById = async (productId) =>{
         try {
             const data = await fs.promises.readFile(this.path,'utf-8')
         
             const products = JSON.parse(data)
-            if(products.find(e => e.id === productId)) {
-                return {status:202,message:"Producto encontrado",value:products.find(e => e.id === productId)}}
+            const value = products.find(e => e.id === productId)
+            if(value) {
+                return {status:202,message:"Producto encontrado",data:value}}
             else {
-                return {status:404,message:"No hay productos con ese ID 404",value:[]}
+                return {status:404,message:"No hay productos con ese ID 404",data:[]}
         }
         } catch (error) {
-            return{status:404,message:"No existen productos",value:[]}
+            return{status:404,message:"No existen productos",data:[]}
             
         }
     }
@@ -98,26 +98,23 @@ export class ProductManager{
                     let index = products.findIndex(e=>e.id===productId)
                     let aux2 = products[index][prop]
                     products[index][prop]=value;
-                    console.log("actualización exitosa 202,\n producto con ID:"+ productId +"\t Su propiedad:" + prop +" \t cambio:"+ aux2 + "------>" + value )
                     await fs.promises.writeFile(this.path,JSON.stringify(products,null,'\t'))
-                return products
+                    return{status:202,message:"actualización exitosa 202,\n producto con ID:"+ productId +"\t Su propiedad:" + prop +" \t cambio:"+ aux2 + "------>" + value,data:[]}
             }
             else{
-                console.log("No existe producto con ese ID 404")
-                return
+                
+                return{status:404,message:"No existe producto con ese ID",data:[]}
             }
 
             
             } catch (error) {
-                console.error("No existen productos")
-            return
+                return{status:404,message:"No existen productos",data:[]}
                     
             }     
         }
 
         else {
-            console.log("Propiedad invalida 405")
-            return
+            return{status:405,message:"Propiedad invalida",data:[]}
         }
     }
 
@@ -128,13 +125,11 @@ export class ProductManager{
        
             if (products.find(e=>e.id===productId)){
                 products.splice(products.findIndex(e=>e.id===productId), 1)
-                console.log("Producto eliminado 202")
                 await fs.promises.writeFile(this.path,JSON.stringify(products,null,'\t'))
-                return products
+                 return{status:202,message:"producto eliminado",data:products}
             }
             else {
-                console.error("No existe producto con ese ID 404")
-                return
+                return{status:404,message:"No existen productos con ese ID",data:[]}
             }
             
         } catch (error) {
